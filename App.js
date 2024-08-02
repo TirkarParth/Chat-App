87e2fbf8220c19c76bcfee7ef44416e201f600a6
TirkarParth <161726354+TirkarParth@users.noreply.github.com>
2024-08-02 04:21:03 +0200

    feat: Initialize Firestore, enable anonymous auth, and implement real-time messaging
    - Created a new Firestore database and updated read/write rules for queries
    - Enabled Anonymous sign-in method
    - Installed firebase@10.3.1
    - Initialized Firebase and Firestore in App.js and passed db to Chat component
    - Implemented anonymous user login in Start.js
    - Initialized Firebase Auth using getAuth()
    - Added signInAnonymously() logic in Start.js onPress event handler
    - Navigated to Chat screen with user id, name, and selected background color on successful anonymous login
    - Implemented real-time message fetching in Chat.js using onSnapshot() and Firestore query
    - Updated onSend function to save messages in Firestore
    - Ensured GiftedChat attaches correct user ID and name to messages
    - Thoroughly commented the code
    - Recorded app demo showing chat functionality on Android emulator

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, LogBox } from 'react-native';
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
 
 // import the screens
 import Start from './components/Start';
 import Chat from './components/Chat';
+
+// import Firestore and Auth
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence, getAuth } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
@@ -13,19 +19,43 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCgcov_f9yGDUbpyAKhOqJmZS8bPdyWRBM",
+    authDomain: "chat-app-8db78.firebaseapp.com",
+    projectId: "chat-app-8db78",
+    storageBucket: "chat-app-8db78.appspot.com",
+    messagingSenderId: "391944198459",
+    appId: "1:391944198459:web:e3ac6cdd05e45916cebf1e",
+    measurementId: "G-J2EJKPB034"
+  };
+
+  // Initialize Firebase if it hasn't been initialized already
+  let app;
+  if (getApps().length === 0) {
+    app = initializeApp(firebaseConfig);
+
+    // Initialize Firebase Auth with persistence
+    initializeAuth(app, {
+      persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+    });
+  } else {
+    app = getApp();
+  }
+
+  // Initialize Firebase Auth
+  const auth = getAuth(app);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Start"
-      >
-        <Stack.Screen
-          name="Start"
-          component={Start}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat">
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/Chat.js
+++ components/Chat.js
@@ -1,61 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { collection, addDoc, onSnapshot, orderBy, query } from "firebase/firestore";
 
-const Chat = ({ route, navigation }) => {
-    const { name, background } = route.params;
+const Chat = ({ route, navigation, db }) => {
+    const { name, background, id } = route.params;
     const [messages, setMessages] = useState([]);
+    const onSend = (newMessages) => {
+      addDoc(collection(db, "messages"), newMessages[0])
+    }
 
+    // Customize speech bubble
+    const renderBubble = (props) => {
+      return <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#757083"
+          },
+          left: {
+            backgroundColor: "#FFF"
+          }
+        }}
+      />
+    }
+
+    // Set user name
     useEffect(() => {
         navigation.setOptions({ title: name });
-
-        setMessages([
-            {
-                _id: 1,
-                text: 'Welcome to the chat!',
-                createdAt: new Date(),
-                system: true,
-            },
-            {
-                _id: 2,
-                text: `Hi ${name}!`,
-                createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: name,
-                },
-            },
-        ]);
     }, []);
 
-    const onSend = (newMessages = []) => {
-        setMessages((previousMessages) =>
-            GiftedChat.append(previousMessages, newMessages)
-        );
-    };
+    // Messages database
+
+    useEffect(() => {
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      const unsubMessages = onSnapshot(q, (documentSnapshot) => {
+          let newMessages = [];
+          documentSnapshot.forEach(doc => {
+            newMessages.push({ 
+              id: doc.id, 
+              ...doc.data(),
+              createdAt: new Date(doc.data().createdAt.toMillis())
+            })
+          });
+          setMessages(newMessages);
+      });
+
+      // Clean up code
+      return () => {
+        if (unsubMessages) unsubMessages();
+      }
+    }, []);
 
     return (
-        <KeyboardAvoidingView
-            style={{ flex: 1 }}
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        >
-            <View style={[styles.container, { backgroundColor: background }]}>
-                <GiftedChat
-                    messages={messages}
-                    onSend={(messages) => onSend(messages)}
-                    user={{
-                        _id: 1,
-                    }}
-                />
-            </View>
-        </KeyboardAvoidingView>
-    );
-};
-
+        <View style={[styles.container, {backgroundColor: background}]}>
+            <GiftedChat
+              messages={messages}
+              renderBubble={renderBubble}
+              onSend={messages => onSend(messages)}
+              user={{
+                //_id: route.params.id,
+                _id: id,
+                name
+              }}
+            />
+            { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+        </View>
+        )
+}
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+ container: {
+  flex: 1,
+ },
 });
-
 export default Chat;
